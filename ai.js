bc440ef37e864880607972d13a790ec88d25ba30
Jose Quintana <joseguardoq@gmail.com>
2024-12-25 22:58:32 -0500

    Add env variables

--- ai.js
+++ ai.js
@@ -7,7 +7,7 @@ You are an assistant that receives a list of ingredients that a user has and sug
 const anthropic = new Anthropic({
   // Make sure you set an environment variable in Scrimba
   // for ANTHROPIC_API_KEY
-  apiKey: process.env.ANTHROPIC_API_KEY,
+  apiKey: import.meta.process.env.ANTHROPIC_API_KEY,
 
   dangerouslyAllowBrowser: true,
 })
@@ -31,7 +31,7 @@ export async function getRecipeFromChefClaude(ingredientsArr) {
 
 // Make sure you set an environment variable in Scrimba
 // for HF_ACCESS_TOKEN
-const hf = new HfInference(process.env.HF_ACCESS_TOKEN)
+const hf = new HfInference(import.meta.process.env.HF_ACCESS_TOKEN)
 
 export async function getRecipeFromMistral(ingredientsArr) {
   const ingredientsString = ingredientsArr.join(', ')
